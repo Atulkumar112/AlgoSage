d0049e0038ecdb5ea789e7cb44d01ac155124514
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-08-04 17:25:47 +0530

    Update pattern3.java

--- Recursion/pattern3.java
+++ Recursion/pattern3.java
@@ -29,3 +29,30 @@ public class practicing{
     }
   }
 }
+
+//with Recursion 
+public class practicing{
+  static void firstPattern(int n){
+    if(n==0){
+      return;
+    }
+    for(int i=1; i<=n; i++){
+      System.out.print(" "+ i);
+    }
+    System.out.println();
+    
+    firstPattern(n-1);
+    
+    
+    for(int j=1; j<=n; j++){
+      System.out.print(" "+ j);
+    }
+    System.out.println();
+    
+    
+  }
+  public static void main(String[] args) {
+    int n = 5;
+    firstPattern(n);
+  }
+}

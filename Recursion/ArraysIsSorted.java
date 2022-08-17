32256cda37ad7c26d92ee032ea15f5e057392ed2
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-08-17 21:55:18 +0530

    Create ArraysIsSorted.java
    ex1:
    arr = {1,2,3,4,5}
    true
    
    ex2:-
    arr = {1,2,3,4,4}
    false
    
    ex3:-
    arr = {1,2,6,3,2}
    false

---
+++ Recursion/ArraysIsSorted.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Practice {
+  public static boolean checkReverse(int[] arr, int len, int i){
+    if(i == len-1){
+      return true;
+    }
+    
+    if(arr[i] < arr[i+1]){
+      return checkReverse(arr, len, i+1);
+    }
+    else{
+      return false;
+    }
+  }
+  public static void main(String[] args) {
+    int[] arr = {1,2,3,4,4};
+    System.out.println(checkReverse(arr, arr.length, 0));
+  }
+}

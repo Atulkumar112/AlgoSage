8f5552ffd8bab7b449c5445f10e3e5df46a83e4a
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-09-29 20:09:21 +0530

    Create EASYSortanarrayof0s,1sand2s.java
    Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
    
    
    Example 1:
    
    Input: 
    N = 5
    arr[]= {0 2 1 2 0}
    Output:
    0 0 1 2 2
    Explanation:
    0s 1s and 2s are segregated 
    into ascending order.
    Example 2:
    
    Input: 
    N = 3
    arr[] = {0 1 0}
    Output:
    0 0 1
    Explanation:
    0s 1s and 2s are segregated 
    into ascending order.
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
    
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(1)
    
    
    Constraints:
    1 <= N <= 10^6
    0 <= A[i] <= 2
    
    
    Company Tags-->
    Adobe Amazon Hike MakeMyTrip MAQ Software Microsoft Morgan Stanley Ola Cabs OYO Rooms Paytm Qualcomm SamsungSAP Labs Snapdeal Walmart Yatra.com Flipkart

---
+++ GFG/EASYSortanarrayof0s,1sand2s.java
@@ -0,0 +1,13 @@
+//MOST important questions in a easy list
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here
+        Arrays.sort(a);
+        for(int i=0; i<n; i++){
+            System.out.print("");
+        }
+    }
+}

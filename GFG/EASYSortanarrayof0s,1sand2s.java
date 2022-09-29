6575c3fe4108501f7c9b6b50893ad23e0d6edc4c
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-09-30 00:40:56 +0530

    Update EASYSortanarrayof0s,1sand2s.java
    Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
    
    
    Example 1:
    
    Input: 
    N = 5
    arr[]= {0 2 1 2 0}
    Output:
    0 0 1 2 2
    Explanation:
    0s 1s and 2s are segregated 
    into ascending order.
    Example 2:
    
    Input: 
    N = 3
    arr[] = {0 1 0}
    Output:
    0 0 1
    Explanation:
    0s 1s and 2s are segregated 
    into ascending order.
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
    
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(1)
    
    
    Constraints:
    1 <= N <= 10^6
    0 <= A[i] <= 2

--- GFG/EASYSortanarrayof0s,1sand2s.java
+++ GFG/EASYSortanarrayof0s,1sand2s.java
@@ -1,5 +1,46 @@
 //MOST important questions in a easy list
+//The best solution is based on linear approach which is given below 
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        
+        int low = 0 , mid = 0 , high = n-1;
+        
+        while(mid <= high)
+        {
+            
+            if(a[mid] == 0){
+                
+                int temp = a[mid];
+                a[mid] = a[low];
+                a[low] = temp;
+                
+                mid++;
+                low++;
+            }
+            
+            else if(a[mid] == 1){
+                
+                mid++;
+            }
+            
+            else{
+                
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                
+                high--;
+            }
+        }
+    }
+}
 
+//--------------------------------------------------------------
+//below solution is fine and passing all test cases but if you do this same code as in front of the interview he will instantly 
+//reject you 
 class Solution
 {
     public static void sort012(int a[], int n)

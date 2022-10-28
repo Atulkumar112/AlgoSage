f75e50a6964500e4d6391a9c95404a56cec8c2c6
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-10-29 04:38:58 +0530

    Create 03. String palindrome.java

---
+++ Important DSA/This file contains 50 code answer of above 50 question  file/03. String palindrome.java
@@ -0,0 +1,17 @@
+package com.cg.java;
+
+import java.util.*;
+
+public class LC {
+	public static void main(String[] args) {
+		System.out.println("Enter a String:");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String ans ="";
+		for(int i=s.length()-1; i>=0; i--) {
+			ans+= s.charAt(i);
+		}
+		
+		System.out.println(ans);
+	}
+}

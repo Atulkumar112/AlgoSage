458c3f18ec279c3bab81a4593446af17a9ff06c6
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-10-30 20:15:44 +0530

    Create ImplementationOfQueueWithTheHelpOfTwoStack.java

---
+++ Data Structure/Queue/ImplementationOfQueueWithTheHelpOfTwoStack.java
@@ -0,0 +1,70 @@
+package com.cg.java;
+import java.util.*;
+import java.util.Map.Entry;
+public class Practice {
+	static class Queue{
+		static Stack<Integer> s1 = new Stack<>();
+		static Stack<Integer> s2 = new Stack<>();
+		
+		//isEmpty
+		public static boolean isEmpty() {
+			return s1.isEmpty();
+		}
+		
+		// add
+		public static void add(int data) {
+			if(isEmpty()) {
+				s1.push(data);
+				return;
+			}
+			
+			while(!s1.isEmpty()) {
+				s2.push(s1.pop());
+			}
+			
+			s1.push(data);
+			
+			while(!s2.isEmpty()) {
+				s1.push(s2.pop());
+			}
+		}
+		
+		public static int remove() {
+			if(s1.isEmpty()) {
+				System.out.println("Queue is empty");
+				return -1;
+			}
+			
+			int res = s1.pop();
+			return res;
+		}
+		
+		public static int peek() {
+			if(s1.isEmpty()) {
+				System.out.println("Queue is empty");
+				return -1;
+			}
+			
+			return s1.peek();
+			
+		}
+		
+	}
+	
+	public static void main(String args[]) {
+		Queue q = new Queue();
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		q.add(5);
+				
+		
+		while(!q.isEmpty()) {
+			System.out.println(q.peek());
+			q.remove();
+
+		}
+			
+	}
+}

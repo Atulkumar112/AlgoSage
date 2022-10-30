c9e708dec4cde4503b8901a07178bc967a101129
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-10-30 22:41:15 +0530

    Create CountTheNodes.java

---
+++ Data Structure/Tree/CountTheNodes.java
@@ -0,0 +1,60 @@
+package com.cg.java;
+
+import java.util.*;
+
+public class Practice {
+      
+  //Defining a tree node with created a nested class
+      static class Node{
+          int data;
+          Node left;
+          Node right;
+          
+          Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+          }
+      }
+      
+      
+      static int i=-1;
+      public static Node buildTree(int[] arr){
+        
+        i++;
+        if(arr[i] == -1) return null;
+        Node newNode = new Node(arr[i]);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
+        
+        return newNode;
+      }
+    
+      public static int countNodes(Node root) {
+    	  if(root == null) {
+    		  return 0;
+    	  }
+    	  int leftNodes = countNodes(root.left);
+    	  int rightNodes = countNodes(root.right);
+    	  return leftNodes + rightNodes + 1;
+      }
+    
+      // main method
+      public static void main(String[] args) {
+        /*
+              1 
+            /   \
+          2      3
+        /  \      \
+      4     5      6
+        
+        */
+      int[] arr = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+      Node root = buildTree(arr);     
+      System.out.println("The total nodes are: "+countNodes(root));
+  }
+}
+
+
+//-------------------------------------Output------------------
+The total nodes are: 6

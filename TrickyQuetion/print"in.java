1539418d98f105aae8d37f6244bdb5a433b7d753
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-11-03 13:15:11 +0530

    Create print"in.java
    print the " (double inverted comma ) comma

---
+++ TrickyQuetion/print"in.java
@@ -0,0 +1,15 @@
+// print the " (double inverted comma ) comma 
+package com.cg.java;
+
+public class GroupDis {
+
+	public static void main(String[] args) {
+		System.out.println('"');
+    //----------------------------OR
+    char ch = '"';
+    System.out.println(ch);
+    
+    //----------------------------OR
+    System.out.println("\"");
+	}
+}

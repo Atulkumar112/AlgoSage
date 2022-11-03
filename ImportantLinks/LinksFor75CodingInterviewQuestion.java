742bf1dff73aaa9c591ab9f07d3a01b2dfb806f7
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-11-03 23:12:01 +0530

    Create LinksFor75CodingInterviewQuestion.java

---
+++ ImportantLinks/LinksFor75CodingInterviewQuestion.java
@@ -0,0 +1,6 @@
+/*
+Please go through the below link.. 
+these 75 question covers all the majar question which is also most important 
+*/
+
+https://www.java67.com/2018/05/top-75-programming-interview-questions-answers.html

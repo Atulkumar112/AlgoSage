b80c2060cd641591b2d7d752117ea6b086760de3
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-11-15 22:13:47 +0530

    Create Medium222. Count Complete Tree Nodes.java
    Given the root of a complete binary tree, return the number of the nodes in the tree.
    
    According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
    
    Design an algorithm that runs in less than O(n) time complexity.
    
     
    
    Example 1:
    
    
    Input: root = [1,2,3,4,5,6]
    Output: 6
    Example 2:
    
    Input: root = []
    Output: 0
    Example 3:
    
    Input: root = [1]
    Output: 1
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [0, 5 * 104].
    0 <= Node.val <= 5 * 104
    The tree is guaranteed to be complete.

---
+++ Leetcode/Medium222. Count Complete Tree Nodes.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public int countNodes(TreeNode root) {
+        if(root == null){
+            return 0;
+        }    
+        
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        
+        if(lh == rh){
+            return (int)Math.pow(2, lh)-1;
+        }
+        
+        return countNodes(root.left) + countNodes(root.right)+1;
+    }
+    
+    public int leftHeight(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return leftHeight(root.left)+1;
+        
+    }
+    
+    public int rightHeight(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return rightHeight(root.right)+1;
+        
+    }
+}

f51ee60f99312b796adc9e5d0d4088eb4e8fdeb7
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-11-21 21:26:24 +0530

    Create MediumUnique partitions.java
    Given a positive integer n, generate all possible unique ways to represent n as sum of positive integers.
    
    Note: The generated output is printed without partitions. Each partition must be in decreasing order. Printing of all the partitions is done in reverse sorted order. 
    
    Example 1:
    
    Input: n = 3
    Output: 3 2 1 1 1 1
    Explanation: For n = 3, 
    {3}, {2, 1} and {1, 1, 1}.
     
    
    Example 2:
    
    Input: n = 4 
    Output: 4 3 1 2 2 2 1 1 1 1 1 1
    Explanation: For n = 4, 
    {4}, {3, 1}, {2, 2}, {2, 1, 1}, {1, 1, 1, 1}.
    
    Your Task:
    You don't need to read or print anything. Your task is to complete the function UniquePartitions() which takes n as input parameter and returns a list of all possible combinations in lexicographically decreasing order. 
     
    
    Expected Time Complexity: O(2^n)
    Expected Space Complexity: O(n)
    
     
    
    Constraints:
    1 <= n <= 25
    
    View Bookmarked Problems 
    Company Tags
    Amazon

---
+++ GFG/MediumUnique partitions.java
@@ -0,0 +1,34 @@
+class Solution
+{
+    public ArrayList<ArrayList<Integer>> UniquePartitions(int n)
+    {
+       ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> curr = new ArrayList<>();
+        
+        int[] arr = new int[n];
+        int idx = n-1;
+        for(int i=0; i<n; i++) {
+        	arr[i] = i+1;        	
+        }
+        
+        dfs(ans, curr, idx, n, arr);
+        
+        return ans;
+    }
+    
+    public void dfs(ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> curr, 
+    int idx, int n, int[] arr){
+        if(idx<0 || n<0){
+            return;
+        }
+        curr.add(arr[idx]);
+        if(arr[idx]-n ==0){
+            ans.add(new ArrayList<>(curr));
+        }
+        
+        dfs(ans, curr, idx, n-arr[idx], arr);
+        curr.remove(curr.size()-1);    // backtracking
+        dfs(ans, curr, idx-1, n, arr);
+        
+    }
+}

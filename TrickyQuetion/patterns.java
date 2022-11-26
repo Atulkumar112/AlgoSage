37448942d761f7f19fcf82fdca49a4314fb13c46
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-11-26 11:30:36 +0530

    Create patterns.java

---
+++ TrickyQuetion/patterns.java
@@ -0,0 +1,70 @@
+package com.cg.java;
+
+import java.util.*;
+
+
+public class GroupDis {	
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter a number:- ");
+//		int n = sc.nextInt();
+		
+		int n=5;
+		
+		/* 
+		* * * * * 
+		* * * 
+		* * 
+		* 
+		for(int i=1; i<n; i++) {
+			for(int j=i; j<n; j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		    
+		*/
+		  
+		
+		/*
+		 	  
+		 
+		* 
+		* * 
+		* * * 
+		* * * * 
+		* * * * * 
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		
+		*/
+		
+		/*
+		
+		     * 
+		    * * 
+		   * * * 
+		  * * * * 
+		 * * * * * 
+		 
+		 
+		 for(int i=1; i<=n; i++){
+            for(int k=n; k>i; k--){
+                System.out.print(" ");     
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*"+" ");        
+            }
+            System.out.println();   
+	     }
+	     
+	     */
+	     
+		
+	}	
+}
+

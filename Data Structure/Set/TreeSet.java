4d7c5baae8b568498d70c3e17857a08140751f52
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-11-30 17:44:48 +0530

    Update TreeSet.java

--- Data Structure/Set/TreeSet.java
+++ Data Structure/Set/TreeSet.java
@@ -6,6 +6,8 @@ import java.util.Map.Entry;
 import javax.print.attribute.standard.NumberOfDocuments;
 
 //TreeSet is implemented with the help of Set interface and it contains only the unique elements and it's now a key value pair.
+//-->The difference between the hashset and treeset is that hashset implemented by map interface whereas treeset implemented by Set interface and 
+//treeset have sorted value and hashset have random value. Otherwise both of them contains only unique element.
 public class GroupDis {	
 	public static void main(String[] args) {	
 		TreeSet<Integer> ts = new TreeSet<>();

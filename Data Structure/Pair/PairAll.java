e7de98b37d93079c6abdb7b4f1ec9a9fb181db31
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-12-01 19:00:42 +0530

    Create PairAll.java

---
+++ Data Structure/Pair/PairAll.java
@@ -0,0 +1,15 @@
+//-->Pair is class in java. with the help of this we can add a pair 
+Pair<String, Integer> p = new Pair<String, Integer>("Atul", 123);
+
+--> // we u want to access key or value in pair then use 
+p.getKey();
+p.getValue();
+
+//--> we want to add mulitple pair then you can use 
+List<Pair<Integer, String>> pairList = new ArrayList<>();
+pairList.add(new Pair<String,Integer> ("atul",1));
+pairList.add(new Pair<String,Integer> ("ujjwal",2));
+pairList.add(new Pair<String,Integer> ("shayla",3));
+pairList.add(new Pair<String,Integer> ("utsav",5));
+
+-->

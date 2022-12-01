f819e0aa5aa5ed97f2d093cf524c8a61db4d1b7f
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-12-01 18:11:23 +0530

    Update Easy1704. Determine if String Halves Are Alike.java
    You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
    
    Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
    
    Return true if a and b are alike. Otherwise, return false.
    
     
    
    Example 1:
    
    Input: s = "book"
    Output: true
    Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
    Example 2:
    
    Input: s = "textbook"
    Output: false
    Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
    Notice that the vowel o is counted twice.
     
    
    Constraints:
    
    2 <= s.length <= 1000
    s.length is even.
    s consists of uppercase and lowercase letters.

--- Leetcode/Easy1704. Determine if String Halves Are Alike.java
+++ Leetcode/Easy1704. Determine if String Halves Are Alike.java
@@ -1,20 +1,39 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
+        
+        // my code given 19ms
         String a = s.substring(0, s.length()/2);
-      String b = s.substring(s.length()/2);
-      int ans=0;
-
-      List<Character> list = Arrays.asList('a','e','i','o','u','A','E','I','O','U');
-
-      for(int i=0; i<a.length(); i++) {
-
-        if(list.contains(a.charAt(i))){
-          ans++;
-        }
-        if(list.contains(b.charAt(i))){
-          ans--;
-        }	
-      }
-      return ans==0;
-      }
+		String b = s.substring(s.length()/2);
+        String vowels = "aeiouAEIOU";
+		int ans=0;
+		
+		// List<Character> list = Arrays.asList('a','e','i','o','u','A','E','I','O','U');
+				
+		for(int i=0; i<a.length(); i++) {
+			
+			// if(list.contains(a.charAt(i))){
+			// 	ans++;
+			// }
+			// if(list.contains(b.charAt(i))){
+			// 	ans--;
+			// }
+            // i changes below statment insted of above then it got good 
+            if(vowels.indexOf(a.charAt(i))>=0) ans++;
+            if(vowels.indexOf(b.charAt(i))>=0) ans--;
+		}
+		return ans==0;
+    }
+    
+    
+    //below is best code getting 2ms time 
+//     String vowels = "aeiouAEIOU";
+    
+//     public boolean halvesAreAlike(String S) {
+//         int mid = S.length() / 2, ans = 0;
+//         for (int i = 0, j = mid; i < mid; i++, j++) {
+//             if (vowels.indexOf(S.charAt(i)) >= 0) ans++;
+//             if (vowels.indexOf(S.charAt(j)) >= 0) ans--;
+//         }
+//         return ans == 0;
+//     }
 }

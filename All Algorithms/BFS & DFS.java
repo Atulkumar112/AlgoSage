2f859be219280ea1bbda168335b21cc040750e81
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-12-06 19:19:35 +0530

    Update BFS & DFS.java

--- All Algorithms/BFS & DFS.java
+++ All Algorithms/BFS & DFS.java
@@ -1,2 +1,3 @@
 // in BFS we see the all posible path (left, right, top and down) and reach the destination. but in DFS we take one way and move forward until the destination and then
 // again takes the another way and then move forword until we reached the destination an so on.
+--> // when you see the minimum distance in graph and matrix question then just use BFS

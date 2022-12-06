2aff1b20a3485bc7eb785afa8d1ca47282b74e6b
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-12-06 19:17:04 +0530

    Create BFS & DFS.java

---
+++ All Algorithms/BFS & DFS.java
@@ -0,0 +1,2 @@
+// in BFS we see the all posible path (left, right, top and down) and reach the destination. but in DFS we take one way and move forward until the destination and then
+// again takes the another way and then move forword until we reached the destination an so on.

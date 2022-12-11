bf1832bd0dc2d02d18687ec4276914d99f83fbce
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-12-11 20:24:48 +0530

    Update AllAboutTheList.java

--- Data Structure/List/AllAboutTheList.java
+++ Data Structure/List/AllAboutTheList.java
@@ -1,2 +1,11 @@
 --> //if you want to add the element in  list or arraylist in a single line then
   List<Integer> list = Arrays.asList(1,21,3,4,5,);    // no need to write list.add(1), list.add(2)
+--> // if we want to the element from the 2D list then we can write code like this
+  public static void main(String[] args) {
+		List<List<Integer>> l = new ArrayList<>();
+		l.add(Arrays.asList(1,2,3));
+		l.add(Arrays.asList(4,5,6));
+		l.add(Arrays.asList(7,8,9));
+		
+		System.out.println(l.get(0).get(2)); // this is most important
+	}

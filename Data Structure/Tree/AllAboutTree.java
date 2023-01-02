1cbc0bc887bef2b88728e7d87541bf46d1ce5784
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-01-02 19:56:54 +0530

    Update AllAboutTree.java

--- Data Structure/Tree/AllAboutTree.java
+++ Data Structure/Tree/AllAboutTree.java
@@ -87,6 +87,33 @@ public class Practice {
 	  		}
 	  	}
       }
+	/* 
+	//--...........................You can also traverse level vise like this.. below is the gfg problem name is "Maximum Value" so here i need to find the 
+	// maximum value from the level in a tree 
+	class Solution {
+    ArrayList<Integer> maximumValue(Node root) {
+        // code is correct but it's showing the time limit exceeded
+       Queue<Node> q = new LinkedList<>();
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		q.add(root);
+		
+		
+		while(!q.isEmpty()) {
+			int currLevelNodeNum = q.size();
+			int max = 0;
+		    while(currLevelNodeNum-->0){
+		        Node d = q.remove();
+		        max = Math.max(max, d.data);
+		        if(d.left != null) q.add(d.left);
+		        if(d.right != null) q.add(d.right);
+		    }
+		    list.add(max);
+		}
+		return list;
+    }
+}
+	*/
       
       // main method
       public static void main(String[] args) {

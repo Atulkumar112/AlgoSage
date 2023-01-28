da491b8d41597d3b648c85e2b5d93436424f07b0
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-01-28 12:33:17 +0530

    Create Quick Sort.java

---
+++ All Algorithms/Sorting Algorithm/Quick Sort.java
@@ -0,0 +1,4 @@
+/*Merge sort is more efficient and works faster than quick sort in case of larger array size or datasets. Quick sort is more efficient and works faster than merge
+sort in case of smaller array size or datasets. Sorting method : The quick sort is internal sorting method where the data is sorted in main memory.
+*/
+

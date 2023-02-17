26ef1bdd2cb022986e317d8dd70f1eb9ec22ad62
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-02-17 17:57:09 +0530

    Create Easy783. Minimum Distance Between BST Nodes.java
    Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
    
     
    
    Example 1:
    
    
    Input: root = [4,2,6,1,3]
    Output: 1
    Example 2:
    
    
    Input: root = [1,0,48,null,null,12,49]
    Output: 1
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [2, 100].
    0 <= Node.val <= 105
     
    
    Note: This question is the same as 530: https://leetcode.com/problems/minimum-absolute-difference-in-bst/

---
+++ Leetcode/Easy783. Minimum Distance Between BST Nodes.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int min = Integer.MAX_VALUE;
+    TreeNode prevNode;
+    public int minDiffInBST(TreeNode root) {
+         inorder(root);
+         return min;
+    }
+
+    public void inorder(TreeNode root){
+        if(root==null){
+            return ;
+        }
+        inorder(root.left);
+        if(prevNode!=null){
+            min = Math.min(min, root.val-prevNode.val);
+        }
+        prevNode = root;
+        inorder(root.right);
+
+    }
+}

e187154443dabaffd05d2dc1311a3882826d4528
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-02-19 22:05:51 +0530

    Create HardFixing Two swapped nodes of a BST.java
    Two of the nodes of a Binary Search Tree (BST) are swapped. Fix (or correct) the BST by swapping them back. Do not change the structure of the tree.
    Note: It is guaranteed than the given input will form BST, except for 2 nodes that will be wrong.
     
    Example 1:
    Input:
           10
         /    \
        5      8
       / \
      2   20
    Output: 1
    Explanation:
     
    Example 2:
    
    Input:
             11
           /    \
          3      17
           \    /
            4  10
    Output: 1 
    Explanation: 
    By swapping nodes 11 and 10, the BST 
    can be fixed.
    Your Task:
    You don't need to take any input. Just complete the function correctBst() that takes root node as parameter. The function should return the root of corrected BST. BST will then be checked by driver code and 0 or 1 will be printed.
    
    Expected Time Complexity : O(Number of nodes)
    Expected Auxiliary Space : O(logN), N = number of nodes
     
    Constraints:
    1 <= Number of nodes <= 10^5
    View Bookmarked Problems 
    Company Tags
    PaytmAmazonMicrosoftFactSetWalmartBankBazaar
    Topic Tags
    Binary Search TreeData Structures

---
+++ GFG/HardFixing Two swapped nodes of a BST.java
@@ -0,0 +1,40 @@
+
+
+/*
+    class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+*/
+
+class Solution {
+    Node first=null; Node second = null;
+    Node prev = null;
+    public Node correctBST(Node root) {
+        fixBst(root);
+        int temp = first.data;
+        first.data = second.data;
+        second.data = temp;
+        
+        return root;
+    }
+    
+    public void fixBst(Node root){
+        if(root==null) return;
+        
+        fixBst(root.left);
+        if(prev!=null && root.data<prev.data){
+            if(first==null) first=prev;
+            second=root;
+        }
+        prev=root;
+        
+        fixBst(root.right);
+    }
+}

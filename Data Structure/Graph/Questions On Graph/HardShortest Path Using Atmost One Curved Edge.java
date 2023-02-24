e4c0d31d6a27f590654e81446f5ffa063c9afd78
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-02-24 16:00:41 +0530

    Update HardShortest Path Using Atmost One Curved Edge.java
    Given an undirected connected graph of n vertices and list of m edges in a graph and for each pair of vertices that are connected by an edge. 
    
    There are two edges between them, one curved edge and one straight edge i.e. the tuple (x, y, w1, w2) means that between vertices x and y, there is a straight edge with weight w1 and a curved edge with weight w2.
    
    You are given two vertices a and b and you have to go from a to b through a series of edges such that in the entire path you can use at most 1 curved edge. Your task is to find the shortest path from a to b satisfying the above condition. If there is no path from a to b, return -1.
    
     
    
    Example 1:
    
    Input:
    n = 4, m = 4
    a = 2, b = 4
    edges = {{1, 2, 1, 4}, {1, 3, 2, 4},
             {1, 4, 3, 1}, {2, 4, 6, 5}}
    Output:
    2
    
    Explanation:
    We can follow the path 2 -> 1 -> 4.
    This gives a distance of 1+3 = 4 if we follow
    all straight paths. But we can take the curved
    path  from 1 -> 4, which costs 1. This
    will result in a cost of 1+1 = 2
    Example 2:
    Input:
    n = 2, m = 1
    a = 1, b = 2
    edges = {{1, 2, 4, 1}}
    Output :
    1
    
    Explanation:
    Take the curved path from 1 to 2 which costs 1. 
    
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function shortestPath() which takes 4 integers n, m, a, and b, and a list of lists named edges of size m as input and returns the cost of shortest path from a to b.
    
    
    Expected Time Complexity: O((m+n)log(n))
    Expected Auxiliary Space: O(n+m)
    
    
    Constraints:
    1 ≤ n,m ≤ 105
    1 ≤ a,b ≤ n
    weight of edges ≤ 104
    
    View Bookmarked Problems 
    Company Tags
    Uber
    Topic Tags
    GraphShortest PathData StructuresAlgorithms

--- Data Structure/Graph/Questions On Graph/HardShortest Path Using Atmost One Curved Edge.java
+++ Data Structure/Graph/Questions On Graph/HardShortest Path Using Atmost One Curved Edge.java
@@ -1,4 +1,4 @@
-// User function Template for Java
+//User function Template for Java
 class Node{
     int v;
     int w;
@@ -7,52 +7,75 @@ class Node{
         this.w = w;
     }
 }
-// below code is written by my hand :)
+
 class Solution {
-    
-    int dis[];
-    int minimumCost(int[][] flights, int n, int k) {
-        dis = new int[n+1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
-      List<List<Node>> adj = new ArrayList<>();
-      
-      for(int i=0; i<=n; i++){
-          adj.add(new ArrayList<>());
-      }
-      
-      for(int[] arr: flights){
-          adj.get(arr[0]).add(new Node(arr[1], arr[2]));
-      }
-      
-      dijkstra(adj, k);
-      
-      int ans=0;
-      for(int i=1; i<dis.length; i++){
-          ans = Math.max(ans, dis[i]);
-      }
-      
-      return ans==Integer.MAX_VALUE ? -1 : ans;
-      
-      
+    static int shortestPath(int n, int m, int a, int b, ArrayList<ArrayList<Integer>> edges) {
+        List<List<Node>> adj = new ArrayList<>();
+        
+        ArrayList<ArrayList<Integer>> curved = new ArrayList<>();
+        
+        for(int i=0; i<=n; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(ArrayList<Integer> list: edges){
+            adj.get(list.get(0)).add(new Node(list.get(1), list.get(2)));
+            adj.get(list.get(1)).add(new Node(list.get(0), list.get(2)));
+            
+            // temp list
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(list.get(0)); temp.add(list.get(1)); temp.add(list.get(3)); // for curved wt
+            curved.add(temp);
+        }
+        
+        int[] disa = dijkstra(adj, a, n);
+        int[] disb = dijkstra(adj, b, n);
+        
+        int ans = disa[b];
+        
+        //consider all poss curved wt at max 1 -> hence itr in each poss
+        for(int i=0; i<m; i++){
+            int u = curved.get(i).get(0);
+            int v = curved.get(i).get(1);
+            int cw = curved.get(i).get(2);
+            
+            ans = Math.min(ans, cw+disa[u]+disb[v]);
+            ans = Math.min(ans, cw+disa[v]+disb[u]);
+        }
+        
+        if(ans>=1000000001) return -1;
+        
+        return ans;
     }
     
-    public void dijkstra(List<List<Node>> adj, int start){
-        // i am just using here custom comparator in PQ for sorting in ascendig order 
-        PriorityQueue<Node> pq = new PriorityQueue<>((x, y) -> {return x.w - y.w; });
+    
+    static int[] dijkstra( List<List<Node>> adj , int start, int n)
+    {
+        // sorting in asc order of src-> target dist
+        int dis[] = new int[n+1];
+        Arrays.fill(dis, 1000000001);
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>((x,y) -> { return x.w - y.w; }); 
+        
         
         dis[start]=0;
         
-        pq.add(new Node(start, 0));  // first para is target and 2nd para is dist from src(2) to target
+        pq.add(new Node(start,0)); //1sr para is target and 2nd para is dist from src(2) to target
         
-        while(!pq.isEmpty()){
-            Node curr_node = pq.poll();
-            for(Node nei: adj.get(curr_node.v)){
-                
-                if(dis[nei.v] > dis[curr_node.v] + nei.w){
+        while(!pq.isEmpty())
+        {
+            Node curr_node =pq.poll();  // VlogV
+            for(Node nei : adj.get(curr_node.v))  // ElogV
+            {
+                // u -> v if(d[v] > d[u] + w[u,v]) d[v] = d[u] + w[u,v])
+                //d[v] = dis[nei.v] , d[u] = dis[curr_node.v]  , w[u,v] = nei.w
+                if(dis[nei.v] > dis[curr_node.v] + nei.w)
+                {
                     dis[nei.v] = dis[curr_node.v] + nei.w;
-                    pq.add(new Node(nei.v, dis[nei.v])); // passing the target and its updated wt
-                }
+                    pq.add(new Node(nei.v , dis[nei.v])); // passing the targer and its updated wt from src(2) to target
+                }   
             }
         }
+        return dis;
     }
-}
+};

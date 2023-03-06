18c9e502611c5c86895b9c6403ec387eef671450
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-03-06 12:31:18 +0530

    Create MediumGeek hates too many 1s.java
    Given an non-negative integer n. You are only allowed to make set bit unset. You have to find the maximum possible value of query so that after performing the given operations, no three consecutive bits of the integer query are set-bits. 
    
    Example 1:
    
    Input:
    n = 2
    Output: 
    2
    Explanation: 
    2's binary form is 10, no 3 consecutive set bits are here. 
    So, 2 itself would be answer.
    Example 2:
    
    Input:
    n = 7
    Output: 
    6
    Explanation: 
    7's binary form is .....00111.We can observe that 3
    consecutive bits are set bits. This is not allowed.
    So, we can perfrom the operation of changing set 
    bit to unset bit. Now, the number 
    becomes 6 that is .....00110. It satifies the 
    given condition. Hence, the maximum possible 
    value is 6.
    
    Your Task:   You don't need to read input or print anything. Your task is to complete the function noConseBits(), which takes integer n as input parameter and returns the maximum value possible so that it satifies the given condition.
    
    Expected Time Complexity: O(1)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    0 ≤ n ≤ 109

---
+++ GFG/MediumGeek hates too many 1s.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public static int noConseBits(int n) {
+        int set[] = new int[32];
+        for(int i=31; i>=0; i--){
+            if((n & (1<<(31-i)))!=0){  // 1<<x means 2^x, 
+            //similary 1<<(31-i) 2^(31-i)
+                set[i]=1;
+            }
+        }
+        
+        int ans = 0;
+        for(int i=0; i<=29; i++){
+            if(set[i]==1){
+                ans+=(1<<(31-i));
+                if(set[i+1] == 1){
+                    set[i+2]=0;
+                }
+            }
+        }
+        if(set[30]==1) ans+=2;
+        if(set[31]==1) ans+=1;
+        
+        return ans;
+    }
+}
+        

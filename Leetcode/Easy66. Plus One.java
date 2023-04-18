a1c8251d2dee5c297572c875247c7eaf707205ec
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-04-18 11:59:18 +0530

    Update Easy66. Plus One.java
    You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
    
    Increment the large integer by one and return the resulting array of digits.
    
     
    
    Example 1:
    
    Input: digits = [1,2,3]
    Output: [1,2,4]
    Explanation: The array represents the integer 123.
    Incrementing by one gives 123 + 1 = 124.
    Thus, the result should be [1,2,4].
    Example 2:
    
    Input: digits = [4,3,2,1]
    Output: [4,3,2,2]
    Explanation: The array represents the integer 4321.
    Incrementing by one gives 4321 + 1 = 4322.
    Thus, the result should be [4,3,2,2].
    Example 3:
    
    Input: digits = [9]
    Output: [1,0]
    Explanation: The array represents the integer 9.
    Incrementing by one gives 9 + 1 = 10.
    Thus, the result should be [1,0].
     
    
    Constraints:
    
    1 <= digits.length <= 100
    0 <= digits[i] <= 9
    digits does not contain any leading 0's.

--- Leetcode/Easy66. Plus One.java
+++ Leetcode/Easy66. Plus One.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-		for(int i=n-1; i>=0; i--){
-			if(digits[i]<9){
-				digits[i]++;
-				return digits;
-			}
-			digits[i]=0;	
+	for(int i=n-1; i>=0; i--){
+		if(digits[i]<9){
+			digits[i]++;
+			return digits;
 		}
-		int[] ans = new int[n+1];
-		ans[0]=1;
-		for(int i=0; i<n; i++){
-			ans[i+1]=digits[i];
-		}
-		return ans;
+		digits[i]=0;	
+	}
+	int[] ans = new int[n+1];
+	ans[0]=1;
+// 	for(int i=0; i<n; i++){          // we have no need to add all digits in ans
+		// 	ans[i+1]=digits[i];		// because it already bydefault 0 in ans arr.
+// 	}
+	return ans;
     }
 }			

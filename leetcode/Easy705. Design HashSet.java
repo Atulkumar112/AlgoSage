a67b6f979028698c3e75704f24cb935db75da209
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-05-31 01:09:25 +0530

    Create Easy705. Design HashSet.java
    Design a HashSet without using any built-in hash table libraries.
    
    Implement MyHashSet class:
    
    void add(key) Inserts the value key into the HashSet.
    bool contains(key) Returns whether the value key exists in the HashSet or not.
    void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
     
    
    Example 1:
    
    Input
    ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
    [[], [1], [2], [1], [3], [2], [2], [2], [2]]
    Output
    [null, null, null, true, false, null, true, null, false]
    
    Explanation
    MyHashSet myHashSet = new MyHashSet();
    myHashSet.add(1);      // set = [1]
    myHashSet.add(2);      // set = [1, 2]
    myHashSet.contains(1); // return True
    myHashSet.contains(3); // return False, (not found)
    myHashSet.add(2);      // set = [1, 2]
    myHashSet.contains(2); // return True
    myHashSet.remove(2);   // set = [1]
    myHashSet.contains(2); // return False, (already removed)
     
    
    Constraints:
    
    0 <= key <= 106
    At most 104 calls will be made to add, remove, and contains.

---
+++ leetcode/Easy705. Design HashSet.java
@@ -0,0 +1,26 @@
+class MyHashSet {
+    Set<Integer> set;
+    public MyHashSet() {
+        this.set = new HashSet<>();
+    }
+    
+    public void add(int key) {
+        set.add(key);
+    }
+    
+    public void remove(int key) {
+        set.remove(key);
+    }
+    
+    public boolean contains(int key) {
+        return set.contains(key) ? true : false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */

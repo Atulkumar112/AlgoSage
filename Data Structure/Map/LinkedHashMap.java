4388e7a825070d9922a5f3ea8d5c73892ee469ae
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-07-20 20:41:37 +0530

    Create LinkedHashMap.java

---
+++ Data Structure/Map/LinkedHashMap.java
@@ -0,0 +1,29 @@
+//LinkedHashMap is very useful when you need to store the key values as a insrting order in map. HashMap and TreeMap are stroed the keys as a random order
+//Example:- 
+class Solution
+{
+    //Function to find the first non-repeating character in a string.
+    static char nonrepeatingCharacter(String s)
+    {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+        
+        for(Map.Entry hm: map.entrySet()){
+            if((int) hm.getValue() == 1){
+                return (char) hm.getKey();
+            }
+            // System.out.println(hm.getKey()+ " --> "+ hm.getValue());
+        }
+        
+        return '$';
+    }
+}
+
+//Output
+// h --> 1
+// e --> 1
+// l --> 2
+// o --> 1
